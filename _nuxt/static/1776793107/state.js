132bc27f9101c20bfd9b8648f213a6757fbe838b
Supin P Surendran <supinps@gmail.com>
2026-04-21 23:08:29 +0530

    Production build update

---
+++ _nuxt/static/1776793107/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1776793107",layout:"default",error:a,serverRendered:true,routePath:b,config:{_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"d2ca0743"}}}}(null,"\u002F"));
