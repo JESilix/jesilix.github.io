2fee5a3fbd34e3db9cd2a36e6a1a1d72976db1b4
Supin P Surendran <supinps@gmail.com>
2026-04-24 09:11:11 +0530

    Production build update

---
+++ _nuxt/static/1777002068/faq/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1777002068",layout:"default",error:a,serverRendered:true,routePath:"\u002Ffaq",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"fe7e7e4c"}}}}(null));
